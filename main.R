12ce0c456c8afa02433e8e3f7706a63c3da9ab65
Freddy Drennan <drennanfreddy@gmail.com>
2021-01-17 01:54:39 -0700

    First commit!

---
+++ main.R
@@ -0,0 +1,9 @@
+# packages <- c(
+#   'purrr', 'dplyr', 'reticulate', 'stringr',
+#   'lubridate', 'usethis', 'roxygen2', 'devtools'
+# )
+#
+# walk(packages, install.packages)
+# walk(packages, usethis::use_package)
+
+library(ndexr)
